61b86d48327f28b6477bcf737c73eb5ed020f8db
matheoisidoro <154465638+matheoisidoro@users.noreply.github.com>
2025-10-24 10:39:27 +0200

    Update variables.c

---
+++ TP1/src/variables.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    int a = 10;
+    unsigned int b = 20;
+    float c = 3.14;
+    double d = 2.71828;
+
+    printf("int : %d\n", a);
+    printf("unsigned int : %u\n", b);
+    printf("float : %.2f\n", c);
+    printf("double : %.5f\n", d);
+
+    return 0;
+}
+
